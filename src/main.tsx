4482cc90d5e009044ea3ef7fde77396e1bf338df
Timothy Tsai <timothy.yt.tsai@gmail.com>
2025-01-25 16:12:12 -0600

    more stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-import { BrowserRouter, Route } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage';
 import Purchases from './pages/Purchases';
 import Payments from './pages/Payments';
@@ -9,9 +9,11 @@ import Login from './pages/Login';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Route path="/" element={<Homepage />} />
-    <Route path="/purchase" element={<Purchases />} />
-    <Route path="/payments" element={<Payments />} />
-    <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/purchase" element={<Purchases />} />
+      <Route path="/payments" element={<Payments />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   </BrowserRouter>
 );
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,23 +1,44 @@
-export default function Homepage()  {
-    return <>
+import { PiWarehouseDuotone } from 'react-icons/pi';
+import { NavLink } from 'react-router';
+
+export default function Homepage() {
+  return (
+    <div className="w-screen h-screen overflow-x-hidden bg-black text-white flex">
+      <div className="w-45/100">
+        <PiWarehouseDuotone />
         <video>
-            {/*  */}
+          <source src={'/supra-video.mp4'} type="video/mp4" />
         </video>
+      </div>
 
-        <ul>
+      <div className="w-55/100 flex flex-col  justify-center items-center px-32 py-16">
+        <div className='flex w-full justify-between items-end mt-auto font-inter'>
+          <ul className='text-6xl gap-16 space-y-2'>
             <li>
-                {/* Home */}
+              {/* Homepage */}
+              <NavLink to="/">Home</NavLink>
             </li>
             <li>
-                {/* Purchases */}
+              {/* Purchases */}
+              <NavLink to="/purchase">Purchase</NavLink>
             </li>
 
             <li>
-                {/* Payments */}
+              {/* Payments */}
+              <NavLink to="/payments">Payment</NavLink>
             </li>
+          </ul>
 
-        </ul>
+          <p className='w-24 italic '>All in one VR shopping experience</p>
+        </div>
 
-    
-    </>
+        <div className="flex gap-64 mt-48 w-full justify-end">
+          <p>
+            Made with love by hackers
+            {/* make this sound better later */}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
